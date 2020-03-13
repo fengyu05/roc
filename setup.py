8297f19609d63e02c6aa048c260db828bbdd87a5
ZF <zdeng@slack-corp.com>
2020-03-13 12:18:41 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 
 setup(
     name='roc-tools',
-    version='0.0.4',
+    version='0.0.5',
     description='Roc charting tools',
     url='https://github.com/fengyu05/roc',
     author='fengyu05',
