11a676a3b26b0031ca2a17da1250c6612b91d0de
ZF <zdeng@slack-corp.com>
2020-03-14 11:11:39 -0700

    Add sampling method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 
 setup(
     name='roc-tools',
-    version='0.0.7',
+    version='0.0.8',
     description='Roc charting tools',
     url='https://github.com/fengyu05/roc',
     author='fengyu05',
